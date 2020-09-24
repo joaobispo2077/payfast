613369f0a1de63d9e50591c7ea4d6b3d5f490166
João Bispo <vitor.vi10@hotmail.com>
2020-09-24 11:47:28 -0300

    :rocket: feat: route post payment & middleware parser

--- src/app/routes/payments.js
+++ src/app/routes/payments.js
@@ -4,4 +4,10 @@ module.exports = (app) => {
         res.status(200).json('00')
     });
 
+    app.post('/pagamentos/pagamento', (req, res) => {
+        const pagamento = req.body;
+        console.log(pagamento);
+        res.status(201).json('created');
+    })
+
 }
--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -5,6 +5,12 @@ const consign = require('consign');
 module.exports = () => {
     const app = express();
 
+    app.use(express.urlencoded({
+        extended: true
+    }));
+
+    app.use(express.json());
+
     consign()
         .include('./src/app/routes')
         .into(app);
